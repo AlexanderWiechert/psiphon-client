bbe769141d074986c97215bf4349a6f36e3e999e
AlexanderWwiechert <awiechert@elastic2ls.com>
2022-08-15 11:06:27 +0200

    added init script and updated configure script

--- configure.sh
+++ configure.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+echo "### install psiphon client ###"
+
 mkdir -p ~/psiphon.client.free
 cd psiphon.client.free
-curl https://github.com/Psiphon-Labs/psiphon-tunnel-core-binaries/raw/master/linux/psiphon-tunnel-core-x86_64 -L -o psiphon-tunnel-core-x86_64
-chmod 755 psiphon-tunnel-core-x86_64
-echo './psiphon-tunnel-core-x86_64 -config client.free.conf' > run.client\nchmod 755 run.client
-./run.client
+curl https://github.com/Psiphon-Labs/psiphon-tunnel-core-binaries/raw/master/linux/psiphon-tunnel-core-x86_64 -L -o /usr/local/bin/psiphon-tunnel-core-x86_64
+chmod 755 /usr/local/bin/psiphon-tunnel-core-x86_64
+
+
+echo "### add init script ###"
+
+INIT_SCRIPT="local.psiphon"
+
+
+cp $INIT_SCRIPT /etc/init.d/
+chmod 755 /etc/init.d/$INIT_SCRIPT
+sudo update-rc.d $INIT_SCRIPT defaults
+chkconfig --add $INIT_SCRIPT
+chkconfig $INIT_SCRIPT on
